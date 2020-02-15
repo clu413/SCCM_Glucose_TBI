b120f5247451f71eef4555dd2754a57ebf341f3f
Chenyue_Lu <chenyuelu95@gmail.com>
2020-02-15 10:42:39 -0500

    Update add_glucose.sql

--- SQL/add_glucose.sql
+++ SQL/add_glucose.sql
@@ -4,3 +4,10 @@ LEFT JOIN `sccm-datathon.team_5.glucose_chart`
 USING(patientunitstayid)
 
 -- save as Patients_w_glucose
+
+SELECT *
+FROM `sccm-datathon.team_5.Patients_w_glucose_plus`
+LEFT JOIN `sccm-datathon.team_5.glucose_cohort_deltas`
+USING(patientunitstayid)
+
+-- save as Patients_w_glucose_and_deltas
